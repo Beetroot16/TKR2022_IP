1d22a4848914b50233bd2aa5dcbaa37ae6395012
Beetroot16 <vishrut.deshmukh@somaiya.edu>
2022-06-15 12:03:54 +0530

    Added contour detection in hardcode

--- Jetson nano/Balldetection_Hardcode.py
+++ Jetson nano/Balldetection_Hardcode.py
@@ -1,160 +1,81 @@
 import cv2
+from cv2 import COLOR_BGR2HSV
 import numpy as np
+import keyboard 
 
-# DEFINITIONS 
+cap = cv2.VideoCapture(1)
 
-cap = cv2.VideoCapture(0)
+#defining main roi
+roimain = np.zeros((480,480,3))
 
-# defining color variables for checker
-red = True # defualt value is red 
+key = None
+
+#defining blue for mask
+lower_blue = np.array([90,100,100])
+upper_blue = np.array([120,355,355])
+
+#kernel
+kernel = np.ones((10,10),np.uint8)
+
+#For imchecker
+red = True
 yellow = False
 green = False
 
-# defining main roi
-roimain = np.zeros((720,1280,3),dtype= np.uint8)
+def image_operations(roimain,kernel):
+    hsv = cv2.cvtColor(roimain,cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv,lower_blue,upper_blue)
+    res = cv2.bitwise_and(roimain,roimain,mask=mask)
 
-start = None
-end = None
+    opening = cv2.morphologyEx(res,cv2.MORPH_OPEN,kernel)
+    edges = cv2.Canny(opening,150,100)
 
-# defining contour coordinates
-x_coordinates_contour = [0,0,0,0]
-y_coordinates_contour = [0,0,0,0]
-
-# defining bounding box  coordinates
-x_coordinates_bbox = [0,0,0,0]
-y_coordinates_bbox = [0,0,0,0]
- 
-# defining blank rois
-roi = np.zeros((720,1280,3),dtype= np.uint8)
-rgbaroi = np.zeros((720,1280,3),dtype= np.uint8)
-roiedges = np.zeros((720,1280,3),dtype= np.uint8)
-roi_gray = np.zeros((720,1280,3),dtype= np.uint8)
-
-# defining frame
-frame = np.zeros((100,100,3),dtype=np.uint8)
-
-midx = 0
-midy = 0
-
-# THRESHOLD VALUES  
-
-area_threshold = 20
-length_threshold = 40
-
-def boundingbox(box):
-    #GLOBAL VARIABLES
-
-    # (2).----------.(3)
-    #   |           |
-    #   |           |
-    #   |           |
-    #   |           |
-    #   |           |
-    # (1).----------.(4)
-
-    global roi , roi_gray , x_coordinates_contour, y_coordinates_contour,frame,start,end,rgbaroi,roiedges,x1roi,x2roi,y1roi,y2roi,red,yellow,green,midx,midy,list
-    # x1
-    x_coordinates_contour[0]= int(box[0][0])
-    # x2
-    x_coordinates_contour[1]= int(box[1][0])
-    # x3
-    x_coordinates_contour[2]= int(box[2][0])
-    # x4
-    x_coordinates_contour[3]= int(box[3][0])
-
-    # y1
-    y_coordinates_contour[0]= int(box[0][1])
-    # y2
-    y_coordinates_contour[1]= int(box[1][1])
-    # y3
-    y_coordinates_contour[2]= int(box[2][1])
-    # y4
-    y_coordinates_contour[3]= int(box[3][1])
-
-    # (x1+x3)/2
-    mid_x1 = ((x_coordinates_contour[0]+x_coordinates_contour[2])/2)
+    cv2.imshow('opening',opening) #for debugging
     
-    # (x2+x4)/2
-    mid_x2 = ((x_coordinates_contour[1]+x_coordinates_contour[3])/2)
+    return edges
 
-     # (y1+y3)/2
-    mid_y1 = ((y_coordinates_contour[0]+y_coordinates_contour[2])/2)
-    
-    # (y2+y4)/2
-    mid_y2 = ((y_coordinates_contour[1]+y_coordinates_contour[3])/2)
-
-    # final contour midpoint
-    midx = (mid_x1+mid_x2)//2
-    midy = (mid_y1+mid_y2)//2
-
-    # weidth and height of contours
-    width = x_coordinates_contour[3]-x_coordinates_contour[0]
-    height = y_coordinates_contour[3]- y_coordinates_contour[2]
-
-    # x1 bounding box
-    # x_coordinates_bbox[0] = int(x_coordinates_contour[0]-width)
-    # x2 bounding box
-    x_coordinates_bbox[1] = int(x_coordinates_contour[1]-width)
-    # x3 bounding box
-    # x_coordinates_bbox[2] = int(x_coordinates_contour[2]+width)
-    # x4 bounding box
-    x_coordinates_bbox[3] = int(x_coordinates_contour[3]+width)
-
-    # y1 bounding box
-    # y_coordinates_bbox[0] = int(y_coordinates_contour[0])
-    # y2 bounding box
-    y_coordinates_bbox[1] = int(y_coordinates_contour[1]-(height*2))
-    # y3 bounding box
-    # y_coordinates_bbox[2] = int(y_coordinates_contour[2]-(height*2))
-    # y4 bounding box
-    y_coordinates_bbox[3] = int(y_coordinates_contour[3])
-
-    start = ( x_coordinates_bbox[1] + x1roi,y_coordinates_bbox[1] + y1roi)
-    end = (x_coordinates_bbox[3] + x1roi,y_coordinates_bbox[3] + y1roi)
-
-    rect = cv2.rectangle(frame,start,end,(0,0,255),3)
-    # if rect:
-    #     count += 1
-    # else:
-
-    # defining roi
-    roi = frame[y_coordinates_bbox[1]+y1roi:y_coordinates_bbox[3]+y1roi,x_coordinates_bbox[1]+x1roi:x_coordinates_bbox[3]+x1roi]
-
-def haarcascade(roi,roi_gray):
+def contour_detection(roimain,edges):
     global red,yellow,green
-    ball_cascade  = cv2.CascadeClassifier(r"C:\Users\Vishr\Desktop\TKR2022_IP\Jetson nano\ball_cascade.xml")
-    ball = ball_cascade.detectMultiScale(roi,1,2)
-    if ball != ():
-        red = False
-        yellow = False
-        green = True
+    if int(cv2.__version__[0])>3:
+            contours,_=cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    else :
+        _,contours,_=cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     
-    for (x,y,w,h) in ball:
-        circle_centre_x = (x+w//2)
-        circle_centre_y = (y+h//2)
+    if contours == ():
+        red = True
+        yellow = False
+        green = False
+    else:
+        red = False
+        yellow = True
+        green = False
 
-        cv2.circle(roi,(circle_centre_x,circle_centre_y),1,(0,0,0),4)
-
-        rect = cv2.rectangle(frame,start,end,(0,255,255),2)
-
-def image_checker(width, height, rgb_color=(0, 0, 0)):
-    # Create black blank image
-    checkerimage = np.zeros((height, width, 3), np.uint8)
-
-    # Since OpenCV uses BGR, convert the color first
-    color = tuple(reversed(rgb_color))
-    # Fill image with color
-    checkerimage[:] = color
-
-    return checkerimage
-
-def E():
-    roitrackbar = frame[170:295,237:394]
-    hsv = cv2.cvtColor(roitrackbar,cv2.COLOR_BGR2HSV)
-    rgba = cv2.cvtColor(roitrackbar, cv2.COLOR_BGR2RGBA)
-    gray = cv2.cvtColor(roitrackbar,cv2.COLOR_BGR2GRAY)
+    for count in contours :
+            area = cv2.contourArea(count)
+            approx = cv2.approxPolyDP(count,0.02*cv2.arcLength(count,True),True)
+            cv2.drawContours(roimain, [approx], 0, (0, 0, 0), 5)
 
 while True:
-    _, frame = cap.read()
+    ret, frame = cap.read()
 
+    roimain = frame #defaultroi 
 
+    if keyboard.is_pressed('1'):  # if key 'q' is pressed 
+        key = 1
+    if keyboard.is_pressed('2'):  # if key 'q' is pressed 
+        key = 2
+    if key == 1:
+        roimain = frame[200:500,200:500]
+    if key == 2:
+        roimain = frame[100:500,100:500]
+
+    edges = image_operations(roimain,kernel)
+    contour_detection(roimain,edges)
+
+    # cv2.imshow('frame',frame)
+    cv2.imshow('roimain',roimain)
+
+    if cv2.waitKey(1) and 0xFF == ('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
