db4e51d93362e26c8423997583f9758ab66cd15d
meetg21 <94597802+meetg21@users.noreply.github.com>
2022-07-09 18:46:40 +0530

    Ab ek hi toh code hai

--- FINAL_CODES/without_serial/R2_seperate.py
+++ FINAL_CODES/without_serial/R2_seperate.py
@@ -1,21 +1,25 @@
 import requests
-from cv2 import imdecode,rectangle,circle,imshow
+import cv2
 from numpy import array,uint8
 
-url = r"http://192.168.211.122:8080/shot.jpg"
+url = r"http://192.168.186.254:8080/shot.jpg"
+while True:
+    online_vid = requests.get(url)
+    online_vid_arr = array(bytearray(online_vid.content),dtype = uint8)
+    online_img = cv2.imdecode(online_vid_arr, -1)
 
-online_vid = requests.get(url)
-online_vid_arr = array(bytearray(online_vid.content),dtype = uint8)
-online_img = imdecode(online_vid_arr, -1)
+    img = online_img
+    image = img
 
-img = online_img
-image = img
+    cv2.rectangle(image, (130,240), (230,600), (0,0,255), 5)
+    cv2.rectangle(image, (330,240), (420,600), (0,0,255), 5)
+    cv2.rectangle(image, (520,240), (610,600), (0,0,255), 5)
+    # cv2.circle(image,(174,130),35,(0,0,0),5)
+    # cv2.circle(image,(345,130),35,(0,0,0),5)
+    # cv2.circle(image,(515,132),35,(0,0,0),5)
 
-rectangle(image, (140,-10), (210,250), (0,0,0), 5)
-rectangle(image, (310,-10), (385,250), (0,0,0), 5)
-rectangle(image, (475,-10), (555,250), (0,0,0), 5)
-circle(image,(174,130),35,(0,0,0),5)
-circle(image,(345,130),35,(0,0,0),5)
-circle(image,(515,132),35,(0,0,0),5)
-
-imshow('image_window',image)
+    cv2.imshow('image_window',image)
+    if cv2.waitKey(1) and 0xFF == ('q'):
+        break
+cv2.cap.release()
+cv2.destroyAllWindows()
