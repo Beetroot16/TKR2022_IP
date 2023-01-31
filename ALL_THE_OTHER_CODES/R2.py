fe711ca30eafd29d361ad46f370fafb863b06357
Beetroot16 <vishrut.deshmukh@somaiya.edu>
2023-02-01 00:56:27 +0530

    just checking

--- ALL_THE_OTHER_CODES/R2.py
+++ ALL_THE_OTHER_CODES/R2.py
@@ -4,6 +4,8 @@ import pickle
 import struct
 import imutils
 
+
+
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host_name  = socket.gethostname()
 host_ip = socket.gethostbyname(host_name)
